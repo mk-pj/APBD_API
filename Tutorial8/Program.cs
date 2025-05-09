c9c628d53d22fad68b2240da3b1807028f53060d
mk-pj <s31209@pjwstk.edu.pl>
2025-05-09 20:32:12 +0200

    adding swagger

--- Tutorial8/Program.cs
+++ Tutorial8/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerUI;
 using Tutorial8.Middlewares;
 using Tutorial8.Repositories;
 using Tutorial8.Services;
@@ -13,6 +15,26 @@ builder.Services.AddScoped<IClientService, ClientService>();
 builder.Services.AddScoped<ITripsRepository, TripsRepository>();
 builder.Services.AddScoped<IClientRepository, ClientRepository>();
 
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Tutorial8",
+        Version = "v1",
+        Description = "Tutorial8 APBD homework",
+        Contact = new OpenApiContact()
+        {
+            Name = "Tutorial8 API Support",
+            Url = new Uri("https://github.com/mk-pj/APBD_API")
+        },
+        License = new OpenApiLicense()
+        {
+            Name = "MIT License",
+            Url = new Uri("https://opensource.org/licenses/MIT")
+        }
+    });
+});
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -23,6 +45,17 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+app.UseSwagger();
+
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Tutorial8");
+    c.DocExpansion(DocExpansion.List);
+    c.DefaultModelExpandDepth(0);
+    c.DisplayRequestDuration();
+    c.EnableFilter();
+});
 
 app.UseGlobalExceptionHandler();
 
